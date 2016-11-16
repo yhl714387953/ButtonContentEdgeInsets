ba782a75123d708492fc28c629a29adb2d1f6ec6
Pactera <pactera@pacterademac-mini-2.local>
2016-11-16 14:37:41 +0800

    Initial Commit

---
+++ ButtonContentEdgeInsets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonContentEdgeInsets
+//
+//  Created by Pactera on 2016/11/16.
+//  Copyright © 2016年 Pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonContentEdgeInsets/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ButtonContentEdgeInsets
+//
+//  Created by Pactera on 2016/11/16.
+//  Copyright © 2016年 Pactera. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
